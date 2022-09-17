1e22bbdd8e65a0d77041922256a6c9d77794eeaa
= <91111745+S1ddh4rthaG@users.noreply.github.com>
2022-09-17 19:15:02 +0530

    [17-9-2022] RTI and Legal Cell Page and Subpages

---
+++ frontend/src/components/RTIView.jsx
@@ -0,0 +1,97 @@
+import { Link } from "react-router-dom";
+export default function RTIView() {
+  //Write your code here
+  return (
+    <div className="d-flex flex-column">
+      <div className="row m-0 p-1">
+        <nav aria-label="breadcrumb">
+          <ol className="breadcrumb">
+            <li className="breadcrumb-item">
+              <Link to="/legal">
+                <span className="btn btn-outline p-0 border-0 link-dark">
+                  RTI & Legal Cell
+                </span>
+              </Link>
+            </li>
+            <li className="breadcrumb-item active" aria-current="page">
+              <Link to="/legal/rti/view">
+                <span className="btn btn-outline p-0 border-0 link-primary">
+                  Legal Cell
+                </span>
+              </Link>
+            </li>
+          </ol>
+        </nav>
+      </div>
+      <div className="row m-0 p-1 justify-content-center">
+        <div className="col-lg-10 col-md-10 col-12 p-2 rounded pb-3">
+          <div className="d-flex flex-column">
+            {/* Element */}
+            <div className="py-2">
+              <div className="card">
+                <div className="card-header d-flex">
+                  <span className="fw-bold flex-fill">
+                    Request ID: <span>(MongoID of Request)</span>
+                  </span>
+                  <span className="fw-bold ms-auto text-success">●</span>
+                </div>
+                <div className="card-body">
+                  <div className="row m-0">
+                    <p>
+                      <span className="fw-bold">Applicant Name: </span>
+                      <span className="ps-1">Siddhartha G</span>
+                    </p>
+                    <p>
+                      <span className="fw-bold">Applicant Email: </span>
+                      <span className="ps-1">cs20b040@iittp.ac.in</span>
+                    </p>
+                    <p>
+                      <span className="fw-bold">Applicant Phone Number: </span>
+                      <span className="ps-1">987654321</span>
+                    </p>
+                    <div className="m-2 border p-2 rounded">
+                      <h5 className="fw-bold mb-2">RTI Request Details:</h5>
+                      <p className="mb-2">
+                        <span className="fw-bold">Query Type: </span>
+                        <span className="ps-1">Medical</span>
+                      </p>
+                      <p>
+                        <span className="fw-bold pe-2">RTI Request:</span>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                        Phasellus faucibus ut sapien ut posuere. Praesent
+                        eleifend nisi erat, vitae faucibus sem pharetra ut.
+                        Praesent tempus metus dolor, non dapibus ipsum maximus
+                        vel. Praesent blandit aliquam lorem eu condimentum.
+                        Suspendisse dictum sed elit vel faucibus. Nulla
+                        venenatis eros ac nunc vehicula, eu finibus felis
+                        eleifend. Cras suscipit leo at interdum bibendum.
+                        Phasellus urna massa, rutrum non purus vitae, aliquet
+                        pretium ante. Sed malesuada velit quis augue mattis
+                        varius. Quisque in arcu a metus imperdiet sodales.
+                        Aliquam nec facilisis quam. Suspendisse potenti.
+                      </p>
+                    </div>
+                    <p>
+                      <span className="fw-bold">Application Status:</span>
+                      <span>
+                        <span className="text-success p-1">Processed</span>
+                      </span>
+                      <span>
+                        <span className="text-primary p-1">Queued</span>
+                      </span>
+                      <span>
+                        <span className="text-danger p-1">
+                          Rejected(Bro u are asking for too much)
+                        </span>
+                      </span>
+                    </p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
--- frontend/src/pages/Legal.jsx
+++ frontend/src/pages/Legal.jsx
@@ -1,25 +1,16 @@
 /*
   Submodule to handle the legal page and Right to Information queries.
 */
-import React, { useState } from "react";
-import RTIForm from "../components/RTIForm";
-import RTIView from "./RTIView";
-
+import React from "react";
+import { Link } from "react-router-dom";
 export default function Legal() {
-  const [click, setClick] = useState(0);
-
   return (
     <div className="d-flex flex-column">
-      <div className={click === 0 ? "row m-0 p-1" : "row m-0 p-1 d-none"}>
+      <div className="row m-0 p-1">
         <h3 className="ps-2 p-1">Right to Information and Legal Cell</h3>
         <h4>Actions</h4>
         <div className="col-lg-6 col-12">
-          <div
-            className="card btn text-start"
-            onClick={() => {
-              setClick(1);
-            }}
-          >
+          <Link to="/legal/rti/create" className="card btn text-start">
             <div className="card-body">
               <h5 className="card-title">Submit an RTI Request!</h5>
               <p>
@@ -27,67 +18,16 @@ export default function Legal() {
                 Request.
               </p>
             </div>
-          </div>
+          </Link>
         </div>
         <div className="col-lg-6 col-12">
-          <div
-            className="card btn text-start"
-            onClick={() => {
-              setClick(2);
-            }}
-          >
+          <Link to="/legal/rti/view" className="card btn text-start">
             <div className="card-body">
               <h5 className="card-title">Legal Cell</h5>
               <p>View, Track and Update the status of RTI Requests.</p>
             </div>
-          </div>
+          </Link>
         </div>
-      </div>
-
-      <div className={click !== 0 ? "row m-0 p-1" : "row m-0 p-1 d-none"}>
-        <nav aria-label="breadcrumb">
-          <ol className="breadcrumb">
-            <li
-              className="breadcrumb-item"
-              onClick={() => {
-                setClick(0);
-              }}
-            >
-              <span className="btn btn-outline p-0 border-0 link-dark">
-                RTI & Legal Cell
-              </span>
-            </li>
-            {click !== 0 && click === 1 ? (
-              <li className="breadcrumb-item active" aria-current="page">
-                <span className="btn btn-outline p-0 border-0 link-primary">
-                  Submit RTI Query
-                </span>
-              </li>
-            ) : null}
-            {click !== 0 && click === 2 ? (
-              <li className="breadcrumb-item active" aria-current="page">
-                <span className="btn btn-outline p-0 border-0 link-primary">
-                  Legal Cell
-                </span>
-              </li>
-            ) : null}
-          </ol>
-        </nav>
-      </div>
-      <div className="row m-0 p-1 justify-content-center">
-      <div className="col-lg-10 col-md-10 col-12 p-2 rounded p-2">
-        <div className="d-flex flex-column">
-          {click !== 0 ? (
-            click === 1 ? (
-              <RTIForm />
-            ) : (
-              <RTIView />
-            )
-          ) : (
-            null
-          )}
-        </div>
-      </div>
       </div>
     </div>
   );
