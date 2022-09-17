b697dffccede06fb155f448e21504878bf7fabef
sirish123 <cs20b043@iittp.ac.in>
2022-09-17 20:08:06 +0530

    [17-09-2022] rti post routes working/postman testing

--- frontend/src/components/CeaAllowanceApplication.jsx
+++ frontend/src/components/CeaAllowanceApplication.jsx
@@ -35,43 +35,43 @@ const CeaAllowanceApplication = () => {
         <div className="col-lg-10 col-md-10 col-12 rounded pb-3">
           <div className="d-flex flex-column">
             <h4 className="text-center mb-3">CEA Allowance Application Form</h4>
-            <form class="needs-validation" novalidate>
-              <div class="alert alert-danger d-none">
+            <form className="needs-validation" novalidate>
+              <div className="alert alert-danger d-none">
                 Please review the problems below:
               </div>
 
-              <div class="row mb-3">
-                <label for="cea-name" class="col-sm-3 col-form-label">
+              <div className="row mb-3">
+                <label for="cea-name" className="col-sm-3 col-form-label">
                   Name
                 </label>
-                <div class="col-sm-9">
+                <div className="col-sm-9">
                   <input
                     type="text"
                     name="name"
-                    class="form-control"
+                    className="form-control"
                     id="cea-name"
                     placeholder="Your name"
                     value={ceaRecord.name}
                     onChange={handleInputs}
                     required
                   />
-                  <div class="invalid-feedback">Name can't be blank</div>
-                  <div class="valid-feedback">Looks good!</div>
-                  <div class="form-text">
+                  <div className="invalid-feedback">Name can't be blank</div>
+                  <div className="valid-feedback">Looks good!</div>
+                  <div className="form-text">
                     Enter Name as on official documents
                   </div>
                 </div>
               </div>
 
-              <div class="row mb-3">
-                <label for="cea-email" class="col-sm-3 col-form-label">
+              <div className="row mb-3">
+                <label for="cea-email" className="col-sm-3 col-form-label">
                   Email Address
                 </label>
-                <div class="col-sm-9">
+                <div className="col-sm-9">
                   <input
                     type="email"
                     name="email"
-                    class="form-control"
+                    className="form-control"
                     id="cea-email"
                     placeholder="Enter your email address"
                     autocomplete="email"
@@ -79,215 +79,215 @@ const CeaAllowanceApplication = () => {
                     onChange={handleInputs}
                     required
                   />
-                  <div class="invalid-feedback">
+                  <div className="invalid-feedback">
                     Email address can't be blank
                   </div>
-                  <div class="valid-feedback">Looks good!</div>
-                  <div class="form-text">
+                  <div className="valid-feedback">Looks good!</div>
+                  <div className="form-text">
                     We'll never share your email with anyone else.
                   </div>
                 </div>
               </div>
 
-              <div class="row mb-3">
-                <label for="cea-phone-number" class="col-sm-3 col-form-label">
+              <div className="row mb-3">
+                <label for="cea-phone-number" className="col-sm-3 col-form-label">
                   Mobile Number
                 </label>
-                <div class="col-sm-9">
+                <div className="col-sm-9">
                   <input
                     type="text"
                     name="phone"
-                    class="form-control"
+                    className="form-control"
                     id="cea-phone-number"
                     placeholder="Your Phone number"
                     value={ceaRecord.phone}
                     onChange={handleInputs}
                     required
                   />
-                  <div class="invalid-feedback">
+                  <div className="invalid-feedback">
                     Phone Number can't be blank
                   </div>
-                  <div class="valid-feedback">Looks good!</div>
-                  <div class="form-text">
+                  <div className="valid-feedback">Looks good!</div>
+                  <div className="form-text">
                     Put country code before your phone number
                   </div>
                 </div>
               </div>
 
-              <div class="row mb-3">
+              <div className="row mb-3">
                 <label
                   for="cea-first-child-name"
-                  class="col-sm-3 col-form-label"
+                  className="col-sm-3 col-form-label"
                 >
                   First Child Name
                 </label>
-                <div class="col-sm-9">
+                <div className="col-sm-9">
                   <input
                     type="text"
                     name="childName1"
-                    class="form-control"
+                    className="form-control"
                     id="cea-first-child-name"
                     placeholder="First Child Name"
                     value={ceaRecord.childName1}
                     onChange={handleInputs}
                     required
                   />
-                  <div class="invalid-feedback">Child Name can't be blank</div>
-                  <div class="valid-feedback">Looks good!</div>
-                  <div class="form-text">
+                  <div className="invalid-feedback">Child Name can't be blank</div>
+                  <div className="valid-feedback">Looks good!</div>
+                  <div className="form-text">
                     Enter Child Name as on official documents.
                   </div>
                 </div>
               </div>
 
-              <div class="row mb-3">
+              <div className="row mb-3">
                 <label
                   for="cea-monthlyCeaDemanded1"
-                  class="col-sm-3 col-form-label"
+                  className="col-sm-3 col-form-label"
                 >
                   Monthly CEA demanded (in ₹)
                 </label>
-                <div class="col-sm-9">
+                <div className="col-sm-9">
                   <input
                     type="number"
                     name="monthlyCeaDemanded1"
-                    class="form-control"
+                    className="form-control"
                     id="cea-monthlyCeaDemanded1"
                     placeholder="Amount in rupees (₹)"
                     value={ceaRecord.monthlyCeaDemanded1}
                     onChange={handleInputs}
                     required
                   />
-                  <div class="invalid-feedback">
+                  <div className="invalid-feedback">
                     Please provide a valid value.
                   </div>
-                  <div class="valid-feedback">Looks good!</div>
+                  <div className="valid-feedback">Looks good!</div>
                 </div>
               </div>
 
-              <div class="row mb-3">
-                <label for="cea-noOfMonths" class="col-sm-3 col-form-label">
+              <div className="row mb-3">
+                <label for="cea-noOfMonths" className="col-sm-3 col-form-label">
                   Number of Months Demanded
                 </label>
-                <div class="col-sm-9">
+                <div className="col-sm-9">
                   <input
                     type="number"
                     name="numberOfMonthsDemanded1"
-                    class="form-control"
+                    className="form-control"
                     id="cea-noOfMonths"
                     placeholder="Number of Months"
                     value={ceaRecord.numberOfMonthsDemanded1}
                     onChange={handleInputs}
                     required
                   />
-                  <div class="invalid-feedback">
+                  <div className="invalid-feedback">
                     Please provide a valid value.
                   </div>
-                  <div class="valid-feedback">Looks good!</div>
+                  <div className="valid-feedback">Looks good!</div>
                 </div>
               </div>
-              <div class="row mb-3">
+              <div className="row mb-3">
                 <label
                   for="cea-second-child-name"
-                  class="col-sm-3 col-form-label"
+                  className="col-sm-3 col-form-label"
                 >
                   Second Child Name
                 </label>
-                <div class="col-sm-9">
+                <div className="col-sm-9">
                   <input
                     type="text"
                     name="childName2"
-                    class="form-control"
+                    className="form-control"
                     id="cea-second-child-name"
                     placeholder="Second Child Name"
                     value={ceaRecord.childName2}
                     onChange={handleInputs}
                   />
-                  <div class="invalid-feedback">Name can't be blank</div>
-                  <div class="valid-feedback">Looks good!</div>
-                  <div class="form-text">
+                  <div className="invalid-feedback">Name can't be blank</div>
+                  <div className="valid-feedback">Looks good!</div>
+                  <div className="form-text">
                     Enter Name as on official documents
                   </div>
                 </div>
               </div>
 
-              <div class="row mb-3">
+              <div className="row mb-3">
                 <label
                   for="cea-monthlyCeaDemanded2"
-                  class="col-sm-3 col-form-label"
+                  className="col-sm-3 col-form-label"
                 >
                   Monthly CEA demanded (in ₹)
                 </label>
-                <div class="col-sm-9">
+                <div className="col-sm-9">
                   <input
                     type="number"
                     name="monthlyCeaDemanded2"
-                    class="form-control"
+                    className="form-control"
                     id="cea-monthlyCeaDemanded2"
                     placeholder="Amount in rupees (₹)"
                     value={ceaRecord.monthlyCeaDemanded2}
                     onChange={handleInputs}
                   />
-                  <div class="invalid-feedback">
+                  <div className="invalid-feedback">
                     Please provide a valid value.
                   </div>
-                  <div class="valid-feedback">Looks good!</div>
+                  <div className="valid-feedback">Looks good!</div>
                 </div>
               </div>
 
-              <div class="row mb-3">
-                <label for="cea-noOfMonths2" class="col-sm-3 col-form-label">
+              <div className="row mb-3">
+                <label for="cea-noOfMonths2" className="col-sm-3 col-form-label">
                   Number of Months Demanded
                 </label>
-                <div class="col-sm-9">
+                <div className="col-sm-9">
                   <input
                     type="number"
                     name="numberOfMonthsDemanded2"
-                    class="form-control"
+                    className="form-control"
                     id="cea-noOfMonths2"
                     placeholder="Number of Months"
                     value={ceaRecord.numberOfMonthsDemanded2}
                     onChange={handleInputs}
                   />
-                  <div class="invalid-feedback">
+                  <div className="invalid-feedback">
                     Please provide a valid value.
                   </div>
-                  <div class="valid-feedback">Looks good!</div>
+                  <div className="valid-feedback">Looks good!</div>
                 </div>
               </div>
 
-              <div class="row mb-3">
-                <div class="col-sm-9 offset-sm-3">
-                  <div class="form-check">
+              <div className="row mb-3">
+                <div className="col-sm-9 offset-sm-3">
+                  <div className="form-check">
                     <input
-                      class="form-check-input"
+                      className="form-check-input"
                       type="checkbox"
                       value=""
                       id="cea-terms"
                       required
                     />
-                    <label class="form-check-label" for="cea-terms">
+                    <label className="form-check-label" for="cea-terms">
                       Review and submit?
                     </label>
-                    <div class="invalid-feedback">Terms must be accepted</div>
-                    <div class="valid-feedback">Looks good!</div>
-                    <div class="form-text">
+                    <div className="invalid-feedback">Terms must be accepted</div>
+                    <div className="valid-feedback">Looks good!</div>
+                    <div className="form-text">
                       Please review again, no changes possible after this
                     </div>
                   </div>
                 </div>
               </div>
 
-              <div class="row mb-0 text-end">
-                <div class="col-sm-9 offset-sm-3">
+              <div className="row mb-0 text-end">
+                <div className="col-sm-9 offset-sm-3">
                   <button
                     type="submit"
-                    class="btn btn-primary m-1"
+                    className="btn btn-primary m-1"
                     onClick={handleSubmit}
                   >
                     Apply!
                   </button>
-                  <button type="reset" class="btn btn-outline-secondary m-1">
+                  <button type="reset" className="btn btn-outline-secondary m-1">
                     Cancel
                   </button>
                 </div>
